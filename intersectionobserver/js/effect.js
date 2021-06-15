a867e4bde76fbd2963682cea314a34e056d709fe
Levente Nyírő <nyiro.levente@gmail.com>
2021-06-15 10:37:28 +0200

    add intersectionobserver folder

---
+++ intersectionobserver/js/effect.js
@@ -0,0 +1,18 @@
+function effect() {
+    const images = document.querySelectorAll('.anim');
+
+    observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                entry.target.style.animation = `anim1 2s ${entry.target.dataset.delay} forwards ease-out`
+            }
+            else {
+                entry.tartget.style.animation = 'none';
+            }
+        });
+    });
+
+    images.forEach(image => {
+        observer.observe(image);
+    });
+}
